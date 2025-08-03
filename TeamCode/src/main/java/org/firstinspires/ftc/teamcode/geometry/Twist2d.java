a8bc02697bf9e476fadc5893d49585edb4bd80d5
polar <git@itzpolar.me>
2025-08-03 13:53:21 -0500

    feat: twist + add controller

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/geometry/Twist2d.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.geometry;
+
+public class Twist2d {
+    public final double dX;
+    public final double dY;
+    public final double dTheta;
+
+    public Twist2d(double dX, double dY, double dTheta) {
+        this.dX = dX;
+        this.dY = dY;
+        this.dTheta = dTheta;
+    }
+}
