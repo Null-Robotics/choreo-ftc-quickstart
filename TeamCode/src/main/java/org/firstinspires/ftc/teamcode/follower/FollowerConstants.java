d98f37de5ac368258ff31b0da36929a2b261ce72
polar <git@itzpolar.me>
2025-08-05 14:36:10 -0500

    constants

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/follower/FollowerConstants.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.follower;
+
+import com.acmerobotics.dashboard.config.Config;
+
+@Config
+public class FollowerConstants {
+    public static final double ACCEL_FACTOR = 0.0;
+
+    public static double TRANSLATIONAL_P = 0.0;
+    public static double TRANSLATIONAL_I = 0.0;
+    public static double TRANSLATIONAL_D = 0.0;
+
+    public static double ANGULAR_P = 0.0;
+    public static double ANGULAR_I = 0.0;
+    public static double ANGULAR_D = 0.0;
+
+    public static double TRANSLATIONAL_VELOCITY_FF_STRENGTH = 0.0;
+    public static double ANGULAR_VELOCITY_FF_STRENGTH = 0.0;
+}
