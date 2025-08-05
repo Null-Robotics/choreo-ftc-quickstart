1e5b6ad5be713b221444e82a757eaa755996f1be
polar <git@itzpolar.me>
2025-08-05 14:27:46 -0500

    velocity

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/VelocityController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/VelocityController.java
@@ -3,9 +3,21 @@ package org.firstinspires.ftc.teamcode.control;
 import org.joml.Vector2d;
 
 public class VelocityController implements Controller<Vector2d> {
+    private final double kP;
+
+    public VelocityController(double kP) {
+        this.kP = kP;
+    }
+
     @Override
     public Vector2d update(Vector2d current, Vector2d setpoint) {
-        return null;
+        double errorX = setpoint.x - current.x;
+        double errorY = setpoint.y - current.y;
+
+        double controlX = kP * errorX;
+        double controlY = kP * errorY;
+
+        return new Vector2d(controlX, controlY);
     }
 
     @Override
