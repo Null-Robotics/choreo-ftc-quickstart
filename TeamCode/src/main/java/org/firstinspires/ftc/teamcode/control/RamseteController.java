a0e0e71bb90c8b2eb2c797fe7f79fb6038cd6e7b
polar <git@itzpolar.me>
2025-08-05 01:45:53 -0500

    add velocity + ramsete

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/RamseteController.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode.control;
+
+import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
+
+public class RamseteController implements Controller<Pose2D> {
+    @Override
+    public Pose2D update(Pose2D current, Pose2D setpoint) {
+        return null;
+    }
+
+    @Override
+    public void reset() {
+
+    }
+}
